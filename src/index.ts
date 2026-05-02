f1c64c762a4b59236e01a675588572888fb6c780
carleom <carleomitolo@gmail.com>
2026-05-02 17:29:54 -0400

    Add switch verification log

--- src/index.ts
+++ src/index.ts
@@ -704,6 +704,8 @@ const main = async () => {
                       path.resolve("data", id, "state.sav"),
                       stSw,
                     );
+                    const activeIdx = wrSw[0x0202406e - 0x02000000] | (wrSw[0x0202406f - 0x02000000] << 8);
+                    console.log("After switch: active battler party slot=" + activeIdx);
                     const { rows: swRows2 } = generateLayout(wrSw, id, 1);
                     await message.channel.send({
                       content:
